95e25f41a83bc4ecfb46fccbad0357782df0f200
Ashwin Mertes <ashwinmertes@telenet.be>
2022-06-25 14:40:34 +0200

    Refactor if/else statements to switches to make code clearer

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,5 @@
 package com.gildedrose;
 
-import java.util.Arrays;
-
 class GildedRose {
     private static final int MINIMUM_ALLOWED_QUALITY = 0;
     private static final int MAXIMUM_ALLOWED_QUALITY = 50;
@@ -12,46 +10,58 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        Arrays.stream(items).forEach(item -> {
-            if (!item.name.equals(ItemName.AGED_BRIE.getName()) && !item.name.equals(ItemName.BACKSTAGE_PASSES.getName())) {
-                if (item.quality > MINIMUM_ALLOWED_QUALITY && !item.name.equals(ItemName.SULFURAS.getName())) {
-                    item.quality = item.quality - 1;
-                }
-            } else {
-                if (item.quality < MAXIMUM_ALLOWED_QUALITY) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(ItemName.BACKSTAGE_PASSES.getName())) {
-                        if (item.sellIn < 11 && item.quality < MAXIMUM_ALLOWED_QUALITY) {
-                            item.quality = item.quality + 1;
-                        }
-
-                        if (item.sellIn < 6 && item.quality < MAXIMUM_ALLOWED_QUALITY) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            }
+        for (Item item : items) {
 
             if (!item.name.equals(ItemName.SULFURAS.getName())) {
-                item.sellIn = item.sellIn - 1;
+                item.sellIn -= 1;
             }
 
-            if (item.sellIn < MINIMUM_ALLOWED_QUALITY) {
-                if (!item.name.equals(ItemName.AGED_BRIE.getName())) {
-                    if (!item.name.equals(ItemName.BACKSTAGE_PASSES.getName())) {
-                        if (item.quality > MINIMUM_ALLOWED_QUALITY && !item.name.equals(ItemName.SULFURAS.getName())) {
-                            item.quality = item.quality - 1;
-                        }
-                    } else {
+            switch (item.name) {
+                case ItemName.Constants.AGED_BRIE_VALUE -> {
+                    increaseQualityByOne(item);
+
+                    if (item.sellIn < MINIMUM_ALLOWED_QUALITY) {
+                        increaseQualityByOne(item);
+                    }
+                }
+                case ItemName.Constants.BACKSTAGE_PASSES_VALUE -> {
+                    increaseQualityByOne(item);
+
+                    if (item.sellIn < 10) {
+                        increaseQualityByOne(item);
+                    }
+
+                    if (item.sellIn < 5) {
+                        increaseQualityByOne(item);
+                    }
+
+                    if (item.sellIn < MINIMUM_ALLOWED_QUALITY) {
                         item.quality = MINIMUM_ALLOWED_QUALITY;
                     }
-                } else {
-                    if (item.quality < MAXIMUM_ALLOWED_QUALITY) {
-                        item.quality = item.quality + 1;
+                }
+                case ItemName.Constants.SULFURAS_VALUE -> {}
+                default -> {
+                    decreaseQualityByOne(item);
+
+                    if (item.sellIn < MINIMUM_ALLOWED_QUALITY) {
+                        decreaseQualityByOne(item);
                     }
                 }
             }
-        });
+
+        }
     }
+
+    private void decreaseQualityByOne(Item item) {
+        if (item.quality > MINIMUM_ALLOWED_QUALITY) {
+            item.quality -= 1;
+        }
+    }
+
+    private void increaseQualityByOne(Item item) {
+        if (item.quality < MAXIMUM_ALLOWED_QUALITY) {
+            item.quality += 1;
+        }
+    }
+
 }
