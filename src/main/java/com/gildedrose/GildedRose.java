7f811df8bfb469a9d34ac8c82fd499f0dc49f787
Ashwin Mertes <ashwinmertes@telenet.be>
2022-06-25 13:14:35 +0200

    Add constants for minimum and maximum allowed quality values

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,6 +3,8 @@ package com.gildedrose;
 import java.util.Arrays;
 
 class GildedRose {
+    private static final int MINIMUM_ALLOWED_QUALITY = 0;
+    private static final int MAXIMUM_ALLOWED_QUALITY = 50;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -12,19 +14,19 @@ class GildedRose {
     public void updateQuality() {
         Arrays.stream(items).forEach(item -> {
             if (!item.name.equals(ItemName.AGED_BRIE.getName()) && !item.name.equals(ItemName.BACKSTAGE_PASSES.getName())) {
-                if (item.quality > 0 && !item.name.equals(ItemName.SULFURAS.getName())) {
+                if (item.quality > MINIMUM_ALLOWED_QUALITY && !item.name.equals(ItemName.SULFURAS.getName())) {
                     item.quality = item.quality - 1;
                 }
             } else {
-                if (item.quality < 50) {
+                if (item.quality < MAXIMUM_ALLOWED_QUALITY) {
                     item.quality = item.quality + 1;
 
                     if (item.name.equals(ItemName.BACKSTAGE_PASSES.getName())) {
-                        if (item.sellIn < 11 && item.quality < 50) {
+                        if (item.sellIn < 11 && item.quality < MAXIMUM_ALLOWED_QUALITY) {
                             item.quality = item.quality + 1;
                         }
 
-                        if (item.sellIn < 6 && item.quality < 50) {
+                        if (item.sellIn < 6 && item.quality < MAXIMUM_ALLOWED_QUALITY) {
                             item.quality = item.quality + 1;
                         }
                     }
@@ -35,17 +37,17 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if (item.sellIn < 0) {
+            if (item.sellIn < MINIMUM_ALLOWED_QUALITY) {
                 if (!item.name.equals(ItemName.AGED_BRIE.getName())) {
                     if (!item.name.equals(ItemName.BACKSTAGE_PASSES.getName())) {
-                        if (item.quality > 0 && !item.name.equals(ItemName.SULFURAS.getName())) {
+                        if (item.quality > MINIMUM_ALLOWED_QUALITY && !item.name.equals(ItemName.SULFURAS.getName())) {
                             item.quality = item.quality - 1;
                         }
                     } else {
-                        item.quality = 0;
+                        item.quality = MINIMUM_ALLOWED_QUALITY;
                     }
                 } else {
-                    if (item.quality < 50) {
+                    if (item.quality < MAXIMUM_ALLOWED_QUALITY) {
                         item.quality = item.quality + 1;
                     }
                 }
