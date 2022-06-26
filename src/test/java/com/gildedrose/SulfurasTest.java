0381034fdb91c3df534444c24d3ec761356578e2
Ashwin Mertes <ashwinmertes@telenet.be>
2022-06-26 13:52:30 +0200

    Quality of Sulfuras is always 80

--- src/test/java/com/gildedrose/SulfurasTest.java
+++ src/test/java/com/gildedrose/SulfurasTest.java
@@ -8,13 +8,13 @@ class SulfurasTest {
 
     @Test
     void qualityAndSellInNeverChanges() {
-        Item item = new Item(ItemName.Constants.SULFURAS, 1, 1);
+        Item item = new Item(ItemName.Constants.SULFURAS, 1, 80);
         Sulfuras sulfuras = new Sulfuras(item);
 
         sulfuras.update();
 
         assertEquals(1, sulfuras.item().sellIn);
-        assertEquals(1, sulfuras.item().quality);
+        assertEquals(80, sulfuras.item().quality);
     }
 
 }
